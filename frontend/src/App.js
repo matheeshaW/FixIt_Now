cae6404c922e7d2b18de7ebc928e93b7a27339c4
Matheesha Weerakoon <matheeshaweerakoon22@gmail.com>
2025-08-26 16:57:50 +0530

    first commit. base files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>FixIt Now - Local Service Marketplace</h1>
+      <p>Welcome to the platform 🚀</p>
+    </div>
+  );
+}
+
+export default App;
+
