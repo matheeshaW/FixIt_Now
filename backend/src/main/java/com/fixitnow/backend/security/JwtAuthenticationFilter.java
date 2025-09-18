e1ef8bfb41111401cda5b08be1f489cc50134bc6
Kanushka Kahakotuwa <kanushkakahakotuwa2001@gmail.com>
2025-09-18 12:17:42 +0530

    change order

--- backend/src/main/java/com/fixitnow/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/fixitnow/backend/security/JwtAuthenticationFilter.java
@@ -1,10 +1,7 @@
 package com.fixitnow.backend.security;
 
-import com.fixitnow.backend.service.CustomUserDetailsService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +10,12 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import com.fixitnow.backend.service.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
