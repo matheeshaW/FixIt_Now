0691544825d2ae17449040f0156b21b83024e2fc
Nirmana Herath <nirmanaherath123@gmail.com>
2025-09-23 10:08:25 +0530

    bug fix

--- backend/src/main/java/com/fixitnow/backend/controller/ServiceController.java
+++ backend/src/main/java/com/fixitnow/backend/controller/ServiceController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
